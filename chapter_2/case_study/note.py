fc2db15b39323990cb921325366b28e4b67db971
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2025-02-20 00:31:40 +0100

    Update note.py

--- chapter_2/case_study/note.py
+++ chapter_2/case_study/note.py
@@ -27,31 +27,31 @@ class Note():
     
     def get_creation_date(self):
         """
-        Returns the creation date of the note
+        Returns the creation date of the note.
         """
         return(self.creation_date)
 
     def get_id(self):
         """
-        Returns the id of the note
+        Returns the id of the note.
         """
         return(self.id)
     
     def get_note(self):
         """
-        Returns the note's content
+        Returns the note's content.
         """
         return(self.note_content)
     
     def get_tags(self):
         """
-        Returns the tags associated with the note
+        Returns the tags associated with the note.
         """
         return(self.tags)
     
-    def add_tags(self, new_tag):
+    def add_tags(self, new_tag, append = True):
         """
-        Adds a new tag to the tags variable
+        Adds a new tag to the tags variable.
 
         Parameters
         ----------
@@ -67,15 +67,15 @@ class Note():
         if (type(new_tag) != str):
             new_tag = str(new_tag)
         
-        # If the tag is the first one to be added, avoid the comma separation
-        if (len(self.tags) != 0):
-            self.tags += ("," + new_tag)
+        if not append or len(self.tags) == 0:
+            self.tags = new_tag
         else:
-            self.tags += new_tag
+            self.tags += ("," + new_tag)
+                
         
     def reset_note(self):
         """
-        Changes the value of the note_content variable to the empty string ""
+        Changes the value of the note_content variable to the empty string "".
 
         Returns
         -------
@@ -83,28 +83,44 @@ class Note():
         """
         self.note_content = ""
     
-    def add_note_content(self, new_content = ""):
+    def add_note_content(self, new_content = "", replace = False):
         """
         Adds the value of the variable new_content to the existing one in 
-        the variable note_content
+        the variable note_content.
 
         Parameters
         ----------
         new_content : str, optional
             New content for the note. The default is "".
+            
+        replace : bool, optional
+            If True replaces the whole content of the note
 
         Returns
         -------
         None.
 
         """
-        self.note_content += new_content
+        if replace:
+            self.note_content = new_content
+        else:
+            self.note_content += new_content
         
         
     def print_note(self):
+        """
+        Prints the content of a note object.
+
+        Returns
+        -------
+        None.
+
+        """
         print("---------------------------------------")
         print("id:", self.get_id())
         print("creation date:", self.get_creation_date())
         print("note content:\n", self.get_note())
         print("tags:", self.get_tags())
         print("---------------------------------------")
+        
+        
